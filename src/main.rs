a47dcc63d0d0b2d236b45fb05722abfaf9a159db
Max Netterberg <max.netterberg@gmail.com>
2025-03-20 21:44:17 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,196 @@
+use dashmap::DashMap;
+use log::{debug, info};
+use ropey::Rope;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::notification::Notification;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+    ast_map: DashMap<String, ()>,
+    document_map: DashMap<String, Rope>,
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult {
+            server_info: None,
+            offset_encoding: None,
+            capabilities: ServerCapabilities {
+                inlay_hint_provider: Some(OneOf::Left(true)),
+                text_document_sync: Some(TextDocumentSyncCapability::Options(
+                    TextDocumentSyncOptions {
+                        open_close: Some(true),
+                        change: Some(TextDocumentSyncKind::FULL),
+                        save: Some(TextDocumentSyncSaveOptions::SaveOptions(SaveOptions {
+                            include_text: Some(true),
+                        })),
+                        ..Default::default()
+                    },
+                )),
+                execute_command_provider: Some(ExecuteCommandOptions {
+                    commands: vec!["dummy.do_something".to_string()],
+                    work_done_progress_options: Default::default(),
+                }),
+                semantic_tokens_provider: Some(
+                    SemanticTokensServerCapabilities::SemanticTokensRegistrationOptions(
+                        SemanticTokensRegistrationOptions {
+                            text_document_registration_options: {
+                                TextDocumentRegistrationOptions {
+                                    document_selector: Some(vec![DocumentFilter {
+                                        language: Some("nrs".to_string()),
+                                        scheme: Some("file".to_string()),
+                                        pattern: None,
+                                    }]),
+                                }
+                            },
+                            semantic_tokens_options: SemanticTokensOptions {
+                                work_done_progress_options: WorkDoneProgressOptions::default(),
+                                legend: SemanticTokensLegend {
+                                    token_types: vec![],
+                                    token_modifiers: vec![],
+                                },
+                                range: Some(true),
+                                full: Some(SemanticTokensFullOptions::Bool(true)),
+                            },
+                            static_registration_options: StaticRegistrationOptions::default(),
+                        },
+                    ),
+                ),
+                definition_provider: Some(OneOf::Left(true)),
+                references_provider: Some(OneOf::Left(true)),
+
+                rename_provider: Some(OneOf::Left(true)),
+                ..ServerCapabilities::default()
+            },
+        })
+    }
+    async fn initialized(&self, _: InitializedParams) {
+        debug!("initialized!");
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+        debug!("file opened");
+        self.on_change(TextDocumentItem {
+            uri: params.text_document.uri,
+            text: &params.text_document.text,
+            version: Some(params.text_document.version),
+        })
+        .await
+    }
+
+    async fn did_change(&self, params: DidChangeTextDocumentParams) {
+        self.on_change(TextDocumentItem {
+            text: &params.content_changes[0].text,
+            uri: params.text_document.uri,
+            version: Some(params.text_document.version),
+        })
+        .await
+    }
+
+    async fn did_save(&self, params: DidSaveTextDocumentParams) {
+        dbg!(&params.text);
+        if let Some(text) = params.text {
+            let item = TextDocumentItem {
+                uri: params.text_document.uri,
+                text: &text,
+                version: None,
+            };
+            self.on_change(item).await;
+            _ = self.client.semantic_tokens_refresh().await;
+        }
+        debug!("file saved!");
+    }
+    async fn did_close(&self, _: DidCloseTextDocumentParams) {
+        debug!("file closed!");
+    }
+
+    async fn inlay_hint(
+        &self,
+        _params: tower_lsp::lsp_types::InlayHintParams,
+    ) -> Result<Option<Vec<InlayHint>>> {
+        debug!("inlay hint");
+
+        Ok(None)
+    }
+
+    async fn did_change_configuration(&self, _: DidChangeConfigurationParams) {
+        debug!("configuration changed!");
+    }
+
+    async fn did_change_workspace_folders(&self, _: DidChangeWorkspaceFoldersParams) {
+        debug!("workspace folders changed!");
+    }
+
+    async fn did_change_watched_files(&self, _: DidChangeWatchedFilesParams) {
+        debug!("watched files have changed!");
+    }
+
+    async fn execute_command(&self, _: ExecuteCommandParams) -> Result<Option<Value>> {
+        debug!("command executed!");
+
+        match self.client.apply_edit(WorkspaceEdit::default()).await {
+            Ok(res) if res.applied => self.client.log_message(MessageType::INFO, "applied").await,
+            Ok(_) => self.client.log_message(MessageType::INFO, "rejected").await,
+            Err(err) => self.client.log_message(MessageType::ERROR, err).await,
+        }
+
+        Ok(None)
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct InlayHintParams {
+    path: String,
+}
+
+#[allow(unused)]
+enum CustomNotification {}
+impl Notification for CustomNotification {
+    type Params = InlayHintParams;
+    const METHOD: &'static str = "custom/notification";
+}
+struct TextDocumentItem<'a> {
+    uri: Url,
+    text: &'a str,
+    version: Option<i32>,
+}
+
+impl Backend {
+    async fn on_change<'a>(&self, params: TextDocumentItem<'a>) {
+        dbg!(&params.version);
+        let rope = ropey::Rope::from_str(params.text);
+        self.document_map
+            .insert(params.uri.to_string(), rope.clone());
+        let diagnostics = vec![];
+
+        self.client
+            .publish_diagnostics(params.uri.clone(), diagnostics, params.version)
+            .await;
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let (stdin, stdout) = (tokio::io::stdin(), tokio::io::stdout());
+
+    let (service, socket) = LspService::new(|client| Backend {
+        client,
+        ast_map: DashMap::new(),
+        document_map: DashMap::new(),
+    });
+
+    info!("Started language server");
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
